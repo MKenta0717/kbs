a1594a833b0674bc45737ff0029eca26b0329b59
MKenta0717 <89497405+MKenta0717@users.noreply.github.com>
2022-04-12 12:35:40 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
-$(window).scroll(function () {
-  estimate_bottom = $(".estimate").height() + $(".estimate").offset().top;
-	scroll_bottom = $(window).scrollTop() + $(window).height();
+// $(window).scroll(function () {
+//   estimate_bottom = $(".estimate").height() + $(".estimate").offset().top;
+// 	scroll_bottom = $(window).scrollTop() + $(window).height();
   
 
 
-  if (scroll_bottom <= estimate_bottom) {
-    $(".fixed").css({
+//   if (scroll_bottom <= estimate_bottom) {
+//     $(".fixed").css({
       
-      position: "fixed",
-      bottom: "0px",
-      background: "rgba(255, 255, 255, 0.8)",
+//       position: "fixed",
+//       bottom: "0px",
+//       background: "rgba(255, 255, 255, 0.8)",
 
-    });
-  } else {
-    $(".fixed").css({
-      position: "absolute",
-      bottom: "0px",
-      background: "pink",
+//     });
+//   } else {
+//     $(".fixed").css({
+//       position: "absolute",
+//       bottom: "0px",
+//       background: "gray",
       
-    });
+//     });
+//   }
+// });
+const estimate = document.querySelector('.estimate');
+const estimate_bottom = estimate.getBoundingClientRect().bottom;
+const fixed = document.querySelector('.fixed');
+
+window.addEventListener('scroll',function(){
+  
+  console.log(window.pageYOffset+window.outerHeight);
+  //console.log(this.document.documentElement.scrollHeight - 28)
+  if(window.pageYOffset+window.outerHeight >= document.documentElement.scrollHeight-28){
+    fixed.classList.add('not_active')
+   
+  }else{
+    fixed.classList.remove('not_active');
   }
 });
