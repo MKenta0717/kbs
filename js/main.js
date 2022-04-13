26d4e791b5e277980e738817498015fedf22d8ee
MKenta0717 <89497405+MKenta0717@users.noreply.github.com>
2022-04-14 00:31:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@ $(window).on("scroll", function() {
  estimate_top = $('.estimate').offset().top;
  estimate_height = $('.estimate').outerHeight();
  customer_bottom = $('.company_footer').outerHeight() + $('.company_footer').offset().top;
- 
- console.log(customer_bottom)
- 
- console.log($(this).scrollTop()+$(this).outerHeight())
+
  if($(this).scrollTop()+$(this).outerHeight() - estimate_height >= customer_bottom){
    $('.estimate').addClass('not_active');
  }else if($('.estimate').hasClass('not_active')){
   $('.estimate').removeClass('not_active');
  }
 });
+
+// ハンバーガーメニュー開閉
+$('.hub_menu').on('click',function(){
+  $(this).toggleClass('is_open');
+  $('.header_nav').toggleClass('is_open');
+});
+
+
